4d3590e26c676467fb13513f381baf449f0333a1
Lucas Giovanella <lucas.giovanella1@universo.univates.br>
2024-09-30 21:37:45 -0300

    título sidebar mobile

--- src/components/admin-panel/sheet-menu.tsx
+++ src/components/admin-panel/sheet-menu.tsx
@@ -28,7 +28,7 @@ export function SheetMenu() {
           >
             <Link href="/dashboard" className="flex items-center gap-2">
               <PanelsTopLeft className="w-6 h-6 mr-1" />
-              <SheetTitle className="font-bold text-lg">Projeto</SheetTitle>
+              <SheetTitle className="font-bold text-lg">LTJ SOFTWARE</SheetTitle>
             </Link>
           </Button>
         </SheetHeader>
